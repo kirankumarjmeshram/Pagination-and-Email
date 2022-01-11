c3fb7a46f130335e357aab2581d51a713227c488
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-11 23:50:38 +0530

    model and controller prepared

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const Product = require("../models/product.model")
+
+const router = express.Router();
+
+router.post("",async (req,res)=>{
+    try{
+        const product = await Product.create(req.body);
+        return res.status(201).send(product);
+    }catch(err){
+        return res.status(500).send(err.message);
+    }
+});
+
+router.get("",async (req,res)=>{
+    try{
+        const products = await Product.find().lean() //.exac();
+        return res.status(201).send(products);
+    }catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
+
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,6 @@
+const app = require("./index");
+const connect = require("./configs/db")
+app.listen(1234,async()=>{
+    await connect();;
+    console.log("listening on 1234")
+})

26068b247b5141ad876f1d60775c650a4609839c
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-19 17:12:03 +0530

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,7 +2,12 @@ const transporter = require("../configs/email");
 
 module.exports = function (from,to,subject,text,html,attachments=null){
     transporter.sendMail({
-        from,to,subject,text,html,attachments
+        from,
+        to,
+        subject,
+        text,
+        html,
+        attachments
     })
 }
 
